cdc16e499057702f0f3e1a9dde09685912131ae8
Rene <rene.talukder@vdabcampus.be>
2025-03-18 11:21:18 +0100

    opbrengst

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -7,17 +7,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GoedeDoelTest {
-	private static final String NAAM = "CliniClowns";
-	private GoedeDoel doel;
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
 
-	@BeforeEach
-	void beforeEach() {
-		doel = new GoedeDoel(NAAM);
-	}
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
 
-	@Test
-	void getNaam() {
-		assertThat(doel.getNaam()).isEqualTo(NAAM);
-	}
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
+
+    @Test
+    void eenNieuweDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
 
 }
